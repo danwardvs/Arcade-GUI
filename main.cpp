12ffb8f41402837a6373c888857251cc88281138
Allan Legemaate <alegemaate@gmail.com>
2015-01-11 21:52:44 -0500

    ICON SiZE CHANGES
    i fix transparency in morning

--- main.cpp
+++ main.cpp
@@ -69,6 +69,9 @@ int step;
 
 int settings[5];
 
+// Scale of icon
+const double icon_scale = 100;
+
 // Background variables
 double background_r, background_g, background_b = 0;
 bool background_r_up, background_g_up, background_b_up = true;
@@ -265,9 +268,23 @@ void draw(){
 
   // Menu
   if(GAME_STATE == MENU){
-    textout_centre_ex( buffer, arimo_22, game[game_focus].name, 512, 150, makecol(0,0,0), -1);
+    // Title
+    textout_centre_ex( buffer, arimo_22, game[game_focus].name, 512, 100, makecol(0,0,0), -1);
+
+    // Draw icon (stretched if needed)
     for (int i = 1; i <= 7; i++){
-      draw_trans_sprite( buffer, game[i].icon, game[i].x - (game_focus*300), game[i].y);
+      // Temporary icon, allows minipulation
+      BITMAP* newIcon = create_bitmap( 200, 200);
+      draw_trans_sprite( newIcon, game[i].icon, 0, 0);
+      int new_scale = 0;
+      // If its the current icon, enlarge it
+      if( i == game_focus)
+        new_scale = icon_scale;
+      // Draw it
+      stretch_sprite(buffer, newIcon,
+        game[i].x - (game_focus * 300) - new_scale/2,
+        game[i].y - new_scale/2,
+        newIcon -> w + new_scale, newIcon -> h + new_scale);
     }
   }
   // Joystick APP
